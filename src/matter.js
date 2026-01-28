2fa3d4b19c23331c822ea8c1b5d8b19e8ca3176b
ishansingh08 <ishansingh453@gmail.com>
2026-01-28 16:31:20 +0530

    NA

--- src/matter.js
+++ src/matter.js
@@ -1,9 +1,3 @@
-{/* <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/matter-js/0.12.0/matter.min.js"></script>
-<script src="https://cdn/jsdelivr.net/npm/matter-wrap@0.2.0/build/matter-wrap.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/matter-attractors@0.1.6/build/matter-attractors.min.js"></script>
- */}
-
 var canvas = document.querySelector("#wrapper-canvas");
 
 var dimensions = {
@@ -37,7 +31,7 @@ function runMatter() {
 
   // create renderer
   var render = Render.create({
-    element: canvas,
+    canvas: canvas,
     engine: engine,
     options: {
       showVelocity: false,
